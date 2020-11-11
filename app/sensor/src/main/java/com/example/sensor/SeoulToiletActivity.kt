9c9141340b61b087eef92bcc1479d205a8729f87
itggangpae <ggangpae1@gmail.com>
2020-11-11 13:06:26 +0900

    1111

--- app/sensor/src/main/java/com/example/sensor/SeoulToiletActivity.kt
+++ app/sensor/src/main/java/com/example/sensor/SeoulToiletActivity.kt
@@ -8,9 +8,8 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.location.Location
 import android.location.LocationManager
-import android.os.AsyncTask
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -26,41 +25,55 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
 
+import kotlinx.android.synthetic.main.activity_seoul_toilet.*
+
 class SeoulToiletActivity : AppCompatActivity() {
     // 런타임에서 권한이 필요한 퍼미션 목록
     val PERMISSIONS = arrayOf(
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION
     )
+
     // 퍼미션 승인 요청시 사용하는 요청 코드
     val REQUEST_PERMISSION_CODE = 1
+
     // 기본 맵 줌 레벨
     val DEFAULT_ZOOM_LEVEL = 17f
+
     // 현재위치를 가져올수 없는 경우 서울 시청의 위치로 지도를 보여주기 위해 서울시청의 위치를 변수로 선언
     // LatLng 클래스는 위도와 경도를 가지는 클래스
     val CITY_HALL = LatLng(37.5662952, 126.97794509999994)
+
     // 구글 맵 객체를 참조할 멤버 변수
     var googleMap: GoogleMap? = null
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        // 맵 초기화
-        initMap()
+    @SuppressLint("MissingPermission")
+    fun getMyLocation(): LatLng {
+        // 위치를 측정하는 프로바이더를 GPS 센서로 지정
+        val locationProvider: String = LocationManager.GPS_PROVIDER
+        // 위치 서비스 객체를 불러옴
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        // 마지막으로 업데이트된 위치를 가져옴
+        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(locationProvider)
+        // 위도 경도 객체로 반환
+        return LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
     }
 
     // 앱에서 사용하는 권한이 있는지 체크하는 함수
     fun hasPermissions(): Boolean {
         // 퍼미션목록중 하나라도 권한이 없으면 false 반환
         for (permission in PERMISSIONS) {
-            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
                 return false
             }
         }
         return true
     }
+
     // ClusterManager 변수 선언
     var clusterManager: ClusterManager<MyItem>? = null
     // ClusterRenderer 변수 선언
@@ -69,7 +82,7 @@ class SeoulToiletActivity : AppCompatActivity() {
     // 맵 초기화하는 함수
     @SuppressLint("MissingPermission")
     fun initMap() {
-        // 맵뷰에서 구글 맵을 불러오는 함수. 컬백함수에서 구글 맵 객체가 전달됨
+        // 맵뷰에서 구글 맵을 불러오는 함수. 콜백함수에서 구글 맵 객체가 전달됨
         mapView.getMapAsync {
             // ClusterManager 객체 초기화
             clusterManager = ClusterManager(this, it)
@@ -89,7 +102,12 @@ class SeoulToiletActivity : AppCompatActivity() {
                     // 현재위치 표시 활성화
                     it.isMyLocationEnabled = true
                     // 현재위치로 카메라 이동
-                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL))
+                    it.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            getMyLocation(),
+                            DEFAULT_ZOOM_LEVEL
+                        )
+                    )
                 }
                 else -> {
                     // 권한이 없으면 서울시청의 위치로 이동
@@ -99,17 +117,15 @@ class SeoulToiletActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    fun getMyLocation(): LatLng {
-        // 위치를 측정하는 프로바이더를 GPS 센서로 지정
-        val locationProvider: String = LocationManager.GPS_PROVIDER
-        // 위치 서비스 객체를 불러옴
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        // 마지막으로 업데이트된 위치를 가져옴
-        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(locationProvider)
-        // 위도 경도 객체로 반환
-        return LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // 맵 초기화
+        initMap()
     }
+
 
     // 현재 위치 버튼 클릭한 경우
     fun onMyLocationButtonClick() {
@@ -117,11 +133,30 @@ class SeoulToiletActivity : AppCompatActivity() {
             hasPermissions() -> googleMap?.moveCamera(
                 CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL)
             )
-            else -> Toast.makeText(applicationContext, "위치사용권한 설정에 동의해주세요", Toast.LENGTH_LONG).show()
+            else -> Toast.makeText(applicationContext, "위치사용권한 설정에 동의해주세요", Toast.LENGTH_LONG)
+                .show()
         }
     }
 
-    // 하단부터 맵뷰의 라이프사이클 함수 호출을 위한 코드들
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seoul_toilet)
+
+        // 맵뷰에 onCreate 함수 호출
+        mapView.onCreate(savedInstanceState)
+        // 앱이 실행될때 런타임에서 위치 서비스 관련 권한체크
+        if (hasPermissions()) {
+            // 권한이 있는 경우 맵 초기화
+            initMap()
+        } else {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
+        }
+        // 현재 위치 버튼 클릭 이벤트 리스너 설정
+        myLocationButton.setOnClickListener { onMyLocationButtonClick() }
+    }
+
+    //맵뷰의 수명 주기 관련 메소드
     override fun onResume() {
         super.onResume()
         mapView.onResume()
@@ -142,144 +177,106 @@ class SeoulToiletActivity : AppCompatActivity() {
         mapView.onLowMemory()
     }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seoul_toilet)
-
-        // 맵뷰에 onCreate 함수 호출
-        mapView.onCreate(savedInstanceState)
-        // 앱이 실행될때 런타임에서 위치 서비스 관련 권한체크
-        if (hasPermissions()) {
-            // 권한이 있는 경우 맵 초기화
-            initMap()
-        } else {
-            // 권한 요청
-            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
-        }
-        // 현재 위치 버튼 클릭 이벤트 리스너 설정
-        myLocationButton.setOnClickListener { onMyLocationButtonClick() }
-    }
-
     // 서울 열린 데이터 광장에서 발급받은 API 키를 입력
     val API_KEY = "6f74435a7367676135384143515370"
-    // 앱이 비활성화될때 백그라운드 작업도 취소하기 위한 변수 선언
-    var task: ToiletReadTask? = null
-    // 서울시 화장실 정보 집합을 저장할 Array 변수. 검색을 위해 저장
-    var toilets = JSONArray()
+
+    //화장실의 위도와 경도 및 이름 들을 저장할 Map 의 List 생성
+    val toiletList: MutableList<Map<String, Any>> = mutableListOf<Map<String, Any>>()
+
     // 화장실 이미지로 사용할 Bitmap
     val bitmap by lazy {
-        val drawable = resources.getDrawable(R.drawable.restroom_sign) as BitmapDrawable
+        val drawable = resources.getDrawable(R.drawable.restroom_sign, null) as BitmapDrawable
         Bitmap.createScaledBitmap(drawable.bitmap, 64, 64, false)
     }
 
-    // JSONArray 를 병합하기 위해 확장함수 사용
-    fun JSONArray.merge(anotherArray: JSONArray) {
-        for (i in 0 until anotherArray.length()) {
-            this.put(anotherArray.get(i))
-        }
-    }
-
-    // 화장실 정보를 읽어와 JSONObject 로 반환하는 함수
-    fun readData(startIndex: Int, lastIndex: Int): JSONObject {
-        val url =
-            URL("http://openAPI.seoul.go.kr:8088" + "/${API_KEY}/json/SearchPublicToiletPOIService/${startIndex}/${lastIndex}")
-        val connection = url.openConnection()
-        val data = connection.getInputStream().readBytes().toString(charset("UTF-8"))
-        Log.e("data", data)
-        return JSONObject(data)
-    }
-
-    // 화장실 데이터를 읽어오는 AsyncTask
-    inner class ToiletReadTask : AsyncTask<Void, JSONArray, String>() {
-        // 데이터를 읽기 전에 기존 데이터 초기화
-        override fun onPreExecute() {
-            // 구글맵 마커 초기화
-            googleMap?.clear()
-            // 화장실 정보 초기화
-            toilets = JSONArray()
-        }
-
-        override fun doInBackground(vararg params: Void?): String {
-            // 서울시 데이터는 최대 1000 개씩 가져올수 있기 때문에
-            // step 만큼 startIndex 와 lastIndex 값을 변경하며 여러번 호출해야 함.
-            val step = 1000
-            var startIndex = 1
-            var lastIndex = step
-            var totalCount = 0
+    //다운로드 받아서 파싱할 스레드
+    inner class ToiletThread : Thread() {
+        override fun run() {
+            //데이터의 시작과 종료 인덱스
+            var startIdx = 1
+            var endIdx = 1000
+            //데이터의 전체 개수를 저장하기 위한 프로퍼티
+            var count = 0
             do {
-                // 백그라운드 작업이 취소된 경우 루프를 빠져나간다.
-                if (isCancelled) break
-                // totalCount 가 0 이 아닌 경우 최초 실행이 아니므로 step 만큼 startIndex 와 lastIndex 를 증가
-                if (totalCount != 0) {
-                    startIndex += step
-                    lastIndex += step
+                //파싱할 URL 생성
+                var url =
+                    URL("http://openAPI.seoul.go.kr:8088" + "/${API_KEY}/json/SearchPublicToiletPOIService/${startIdx}/${endIdx}")
+                //연결해서 문자열 가져오기
+                val connection = url.openConnection()
+                val data = connection.getInputStream().readBytes().toString(charset("UTF-8"))
+                //JSON 파싱
+                val jsonData = JSONObject(data)
+                val root = jsonData.getJSONObject("SearchPublicToiletPOIService")
+                if (count == 0) {
+                    count = root.getInt("list_total_count")
                 }
-                // startIndex, lastIndex 로 데이터 조회
-                val jsonObject = readData(startIndex, lastIndex)
-                // totalCount 를 가져온다.
-                totalCount = jsonObject.getJSONObject("SearchPublicToiletPOIService").getInt("list_total_count")
-                // 화장실 정보 데이터 집합을 가져온다.
-                val rows = jsonObject.getJSONObject("SearchPublicToiletPOIService").getJSONArray("row")
-                // 기존에 읽은 데이터와 병합
-                toilets.merge(rows)
-                // UI 업데이트를 위해 progress 발행
-                publishProgress(rows)
-            } while (lastIndex < totalCount) // lastIndex 가 총 개수보다 적으면 반복한다
-            return "complete"
-        }
+                val row = root.getJSONArray("row")
+                for (i in 0 until row.length()) {
+                    val obj = row.getJSONObject(i)
+                    val map = mutableMapOf<String, Any>()
+                    map.put("FNAME", obj.getString("FNAME"))
+                    map.put("ANAME", obj.getString("ANAME"))
+                    map.put("Lat", obj.getDouble("Y_WGS84"))
+                    map.put("Lng", obj.getDouble("X_WGS84"))
 
-        // 데이터를 읽어올때마다 중간중간 실행
-        override fun onProgressUpdate(vararg values: JSONArray?) {
-            // vararg 는 JSONArray 파라미터를 가변적으로 전달하도록 하는 키워드
-            // 인덱스 0의 데이터를 사용
-            val array = values[0]
-            array?.let {
-                for (i in 0 until array.length()) {
-                    // 마커 추가
-                    addMarkers(array.getJSONObject(i))
+                    toiletList.add(map)
                 }
+                //인덱스를 변경해서 데이터 계속 가져오기
+                startIdx = startIdx + 1000
+                endIdx = endIdx + 1000
+            } while (startIdx < count)
+            handler.sendEmptyMessage(0)
+        }
+    }
+
+    val handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            Log.e("toiletList", toiletList.toString())
+            for (map in toiletList) {
+                addMarkers(map as MutableMap<String, Any>)
             }
             // clusterManager 의 클러스터링 실행
             clusterManager?.cluster()
+
         }
     }
 
-    // 앱이 활성화될때 서울시 데이터를 읽어옴
-    override fun onStart() {
-        super.onStart()
-        task?.cancel(true)
-        task = ToiletReadTask()
-        task?.execute()
-    }
-
-    // 앱이 비활성화 될때 백그라운드 작업 취소
-    override fun onStop() {
-        super.onStop()
-        task?.cancel(true)
-        task = null
-    }
 
     // 마커를 추가하는 함수
-    // 마커를 추가하는 함수
-    fun addMarkers(toilet: JSONObject) {
+    fun addMarkers(toilet: MutableMap<String, Any>) {
 //        googleMap?.addMarker(
 //            MarkerOptions()
-//                .position(LatLng(toilet.getDouble("Y_WGS84"), toilet.getDouble("X_WGS84")))
-//                .title(toilet.getString("FNAME"))
-//                .snippet(toilet.getString("ANAME"))
+//                .position(LatLng(toilet.get("Lat") as Double, toilet.get("Lng") as Double))
+//                .title(toilet.get("FNAME") as String)
+//                .snippet(toilet.get("ANAME") as String)
 //                .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
 //        )
         // clusterManager 를 이용해 마커 추가
         clusterManager?.addItem(
             MyItem(
-                LatLng(toilet.getDouble("Y_WGS84"), toilet.getDouble("X_WGS84")),
-                toilet.getString("FNAME"),
-                toilet.getString("ANAME"),
+                LatLng(toilet.get("Lat") as Double, toilet.get("Lng") as Double),
+                toilet.get("FNAME") as String,
+                toilet.get("ANAME") as String,
                 BitmapDescriptorFactory.fromBitmap(bitmap)
             )
         )
     }
 
+    var toiletThread: ToiletThread? = null
 
+    // 앱이 활성화될때 서울시 데이터를 읽어옴
+    override fun onStart() {
+        super.onStart()
+        if (toiletThread == null) {
+            toiletThread = ToiletThread()
+            toiletThread!!.start()
+        }
+    }
+
+    // 앱이 비활성화 될때 백그라운드 작업 취소
+    override fun onStop() {
+        super.onStop()
+        toiletThread!!.isInterrupted
+        toiletThread = null
+    }
 }
